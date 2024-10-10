904b82ce34efe0db79531bc1b83439c5c8cf3c57
Lazar Milic <lazar.milic@alumnos.ump.es>
2024-10-10 17:45:25 +0200

    Commit articles-list httpCall

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor,NgIf } from '@angular/common';
+import { Article } from '../interfaces/article'
+import { NewsService } from '../services/news-service';
 
 @Component({
   selector: 'app-article-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './article-list.component.html',
   styleUrl: './article-list.component.css'
 })
-export class ArticleListComponent {
+export class ArticleListComponent implements OnInit{
 
+  articles: Article[] | undefined;
+  article: Article | null | undefined;
+  message: string | null | undefined;
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit(): void {
+    this.newsService.getArticles().subscribe({
+      next: (data) => {
+        this.articles = data;
+        console.log('getArticles(): ' + JSON.stringify(this.articles));
+      },
+      error: (err) => {
+        console.log('getArticles() error: ' + err);
+      },
+    });
+  }
 }
