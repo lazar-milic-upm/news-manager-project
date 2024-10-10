2f9ed65f314a92a2f712ea084ac96484b0d29444
Gaia De Donatis <gaia.de-donatis3@etu.univ-lorraine.fr>
2024-10-10 19:24:58 +0200

    imange_things

--- src/app/interfaces/article.ts
+++ src/app/interfaces/article.ts
@@ -8,6 +8,8 @@ export interface Article {
     body: string;
     update_date: Date;
     modifiedDate: Date;
-    imageUrl?: string;
+    imageUrl?: string;           
     category: string;
+    image_data?: string;         
+    image_media_type?: string;   
 }
