f07a986ae569500edd06b5e9120fa72c75224f44
Gaia De Donatis <gaia.de-donatis3@etu.univ-lorraine.fr>
2024-10-11 12:32:26 +0200

    filters navigation bar fished

--- src/app/pipes/article-text-filter.pipe.ts
+++ src/app/pipes/article-text-filter.pipe.ts
@@ -6,16 +6,29 @@ import { Article } from '../interfaces/article';
   standalone: true,
 })
 export class ArticleTextFilterPipe implements PipeTransform {
-  transform(articles: Article[], searchText: string): Article[] {
+  transform(articles: Article[], searchText: string, excludes: string[] = []): Article[] {
     if (!articles || !searchText) {
       return articles;
     }
 
     const lowerCaseSearchText = searchText.toLowerCase();
 
-    return articles.filter(article =>
-      article.title.toLowerCase().includes(lowerCaseSearchText) ||
-      article.subtitle.toLowerCase().includes(lowerCaseSearchText)
-    );
+    return articles.filter(article => this.checkInside(article, lowerCaseSearchText, excludes));
+  }
+
+  private checkInside(item: Article, term: string, excludes: string[]): boolean {
+    if (excludes.includes('title') && excludes.includes('subtitle')) {
+      return false;
+    }
+
+    if (!excludes.includes('title') && item.title.toLowerCase().includes(term)) {
+      return true;
+    }
+
+    if (!excludes.includes('subtitle') && item.subtitle.toLowerCase().includes(term)) {
+      return true;
+    }
+
+    return false;
   }
 }
