1f059711a73cf8e77e59999a733f5231cd02258c
Gaia De Donatis <gaia.de-donatis3@etu.univ-lorraine.fr>
2024-10-11 13:23:54 +0200

    log in work in process

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,34 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { User } from '../interfaces/user';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string | null = null;
+  currentUser: User | null = null;
 
   constructor(private router: Router) {}
 
   login() {
-
+    if (this.username && this.password) {
+      this.router.navigate(['/home']); // Navigate to home upon successful login
+    } else {
+      this.errorMessage = 'Please enter a username and password';
+    }
   }
 
-  skipLogin(){
-      this.router.navigate(['/home']);
+  skipLogin() {
+    this.router.navigate(['/home']);
   }
-
 }
