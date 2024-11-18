a12c14fce28c76ce2017e3aad57164cee20ad231
pardisbasiri <pardis.basirii@gmail.com>
2024-11-18 11:32:54 +0100

    first changes for web app

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: false,
+        },
+    });
+
+    mainWindow.loadURL('http://localhost:4200');
+
+    // mainWindow.loadFile(path.join(__dirname, 'dist/news-manager-project/browser/index.html'));
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
